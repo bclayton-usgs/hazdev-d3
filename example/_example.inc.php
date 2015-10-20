a9d817a0ff6777ed4fd16a46a6ca596cece93ef1
Jeremy Fee <jmfee@usgs.gov>
2015-10-20 17:30:46 -0600

    Make template enabled by default on example pages

--- example/_example.inc.php
+++ example/_example.inc.php
@@ -2,7 +2,7 @@
 
 if (!isset($TEMPLATE)) {
   // embed in the template
-  $useTemplate = isset($_GET['template']) && $_GET['template'] === 'true';
+  $useTemplate = !isset($_GET['template']) || $_GET['template'] !== 'false';
   $pageUrl = str_replace(
       strstr($_SERVER['REQUEST_URI'], '?'), '',
       $_SERVER['REQUEST_URI']);
@@ -14,13 +14,15 @@ if (!isset($TEMPLATE)) {
 }
 
 // this is only output during the "content" phase of the template
-$nav = array(
-  navItem($pageUrl . '?template=true', 'View with template'),
-  navItem($pageUrl . '?template=false', 'View without template')
-);
+$nav = array();
 if ($pageUrl !== '/example.php') {
   $nav[] = navItem('/example.php?template=' . $useTemplate, 'Examples Index');
 }
+if ($useTemplate) {
+  $nav[] = navItem($pageUrl . '?template=false', 'View without template');
+} else {
+  $nav[] = navItem($pageUrl . '?template=true', 'View with template');
+};
 
 echo '<nav>', implode(' ', $nav), '</nav>';
 if (!$useTemplate) {
