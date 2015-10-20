73aebf7794d7b5a478dbdae9543428d957b868d7
Jeremy Fee <jmfee@usgs.gov>
2015-10-20 17:08:21 -0600

    Add template, and template dependencies to project.

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -2,21 +2,70 @@
 
 var config = require('./config');
 
+
+var addMiddleware = function (connect, options, middlewares) {
+  var bases,
+      gateway;
+
+  gateway = require('gateway');
+
+  // push in reverse order
+  bases = options.base.slice(0);
+  bases.reverse();
+  bases.forEach(function (base) {
+    middlewares.unshift(gateway(base, {
+      '.php': 'php-cgi',
+      'env': {
+        'PHPRC': 'node_modules/hazdev-template/dist/conf/php.ini'
+      }
+    }));
+  });
+
+  middlewares.unshift(
+    require('compression')({
+      filter: function (req, res) {
+        var type = res.getHeader('Content-Type');
+        return (type+'').match(/(css|javascript)/);
+      }
+    }),
+    require('grunt-connect-proxy/lib/utils').proxyRequest
+  );
+
+  return middlewares;
+};
+
+
 var connect = {
   options: {
     hostname: '*'
   },
+
+  proxies: [
+    {
+      context: '/theme/',
+      host: 'localhost',
+      port: 8003,
+      rewrite: {
+        '^/theme': ''
+      }
+    }
+  ],
+
   dev: {
     options: {
       base: [
-        config.build + '/' + config.src,
-        config.example
+        // example must come first for now, until the following PR is merged:
+        // https://github.com/fgnass/gateway/pull/11
+        config.example,
+        config.build + '/' + config.src
       ],
       livereload: true,
-      open: 'http://localhost:8000/example.html',
+      middleware: addMiddleware,
+      open: 'http://localhost:8000/example.php',
       port: 8000
     }
   },
+
   test: {
     options: {
       base: [
@@ -28,6 +77,7 @@ var connect = {
       port: 8001
     }
   },
+
   dist: {
     options: {
       base: [
@@ -37,6 +87,13 @@ var connect = {
       keepalive: true,
       open: 'http://localhost:8002/example.html',
       port: 8002
+    }
+  },
+
+  template: {
+    options: {
+      base: ['node_modules/hazdev-template/dist/htdocs'],
+      port: 8003
     }
   }
 };
