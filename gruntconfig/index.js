a8b93473128e648077f430b001d0f334a9381a6f
Eric Martinez <emartinez@usgs.gov>
2015-11-05 10:54:39 -0700

    Updated build to use postcss.

--- gruntconfig/index.js
+++ gruntconfig/index.js
@@ -3,28 +3,26 @@
 var config = {
   browserify: require('./browserify'),
   clean: require('./clean'),
-  compass: require('./compass'),
   connect: require('./connect'),
   copy: require('./copy'),
-  cssmin: require('./cssmin'),
   jshint: require('./jshint'),
-  uglify: require('./uglify'),
-  watch: require('./watch'),
   mocha_phantomjs: require('./mocha_phantomjs'),
+  postcss: require('./postcss'),
+  watch: require('./watch'),
+  uglify: require('./uglify'),
 
   tasks: [
     'grunt-browserify',
     'grunt-connect-proxy',
     'grunt-connect-rewrite',
     'grunt-contrib-clean',
-    'grunt-contrib-compass',
     'grunt-contrib-connect',
     'grunt-contrib-copy',
-    'grunt-contrib-cssmin',
     'grunt-contrib-jshint',
     'grunt-contrib-uglify',
     'grunt-contrib-watch',
-    'grunt-mocha-phantomjs'
+    'grunt-mocha-phantomjs',
+    'grunt-postcss'
   ]
 };
 
